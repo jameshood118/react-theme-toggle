214da5f99fa276e4f268207e16bf6e110b12fcce
James Hood <jameshood118@gmail.com>
2020-09-03 20:20:26 -0400

    add css helper to start with dynamic values

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const StyledButton = styled.button`
 border: none;
 padding: 0.5rem 1rem;
 text-transform: uppercase;
-color: ${({primary}) =>(primary ? 'black': '#fff')};
+color: black;
 cursor: pointer;
 outline:none;
+
+${({primary}) => primary && css`
+color: white;
+background: black
+`}
 `
 
 const Button = ({primary, children})=> {
