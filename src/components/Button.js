fe09933c3b9ea8b94123ea780f19022f350c390f
James Hood <jameshood118@gmail.com>
2020-09-03 21:02:17 -0400

    wrapping, getting vscode highlighting

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,6 +5,7 @@ import { Wrapper} from './styled'
 const StyledButton = styled.button`
 border: none;
 padding: 0.5rem 1rem;
+margin: ${({margin})=> margin || '2rem'};
 text-transform: uppercase;
 color: black;
 cursor: pointer;
@@ -16,12 +17,8 @@ background: black
 `}
 `
 
-const Button = ({primary, children})=> {
-return (
-<Wrapper>
-    <StyledButton primary={primary}>{children}</StyledButton>
-</Wrapper>
-    );
+const Button = ({primary, margin, children})=> {
+return (<Wrapper><StyledButton margin={margin} primary={primary}>{children}</StyledButton></Wrapper>);
 }
 
 export default Button;
